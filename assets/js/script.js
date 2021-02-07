092c6a7c56f4cc0b201895fda8254939bc1e07e5
kenwals <kenwals@gmail.com>
2021-02-07 11:32:29 +0000

    feat exit game function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,6 +87,11 @@ class MixOrMatch {
         }, 1000);
     }
 
+    exitGame() {
+        clearInterval(this.countDown);
+        this.hideCards();
+    }
+
     gameOver() {
         clearInterval(this.countDown);
         document.getElementById("game-over-text").classList.add("visible");
@@ -123,29 +128,30 @@ function ready() {
         console.log("you clicked the start button");
         $("#page-home").addClass("collapse");
         $("#page-game").removeClass("collapse");
-         console.log("game page open , home page is collapsed");
-         game.startGame();
+        console.log("game page open , home page is collapsed");
+        game.startGame();
     });
 
     $("#btn-back").click(function(){
         console.log("you clicked go back button");
         $("#page-game").addClass("collapse");
         $("#page-home").removeClass("collapse");
-         console.log("game page collapsed , home page is open");
+        game.exitGame();
+        console.log("game page collapsed , home page is open");
     });
 
     $("#btn-info").click(function(){
         console.log("you clicked the info button");
         $("#page-home").addClass("collapse");
         $("#page-help").removeClass("collapse");
-         console.log("home page collapsed , help page is open");
+        console.log("home page collapsed , help page is open");
     });
 
     $("#btn-exit").click(function(){
         console.log("you clicked the exit button");
         $("#page-help").addClass("collapse");
         $("#page-home").removeClass("collapse");
-         console.log("help page collapsed , home page is open");
+        console.log("help page collapsed , home page is open");
     });
 
     overlays.forEach(overLay => {
