50df74ab1ad43ade5c71dd24191610ab448bb09f
kenwals <kenwals@gmail.com>
2021-02-16 14:46:33 +0000

    feat code init commit

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,1 +1,12 @@
-console.log("leaderboard loaded");
+
+document.getElementById("form-leaderBoard").addEventListener("submit", submitform);
+
+
+
+
+
+function submitform(e){
+    e.preventDefault();
+
+    console.log(123);
+}
